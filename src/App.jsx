1f203664a5fa2bad3dcbbc5a3db92675fd8c67ca
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2024-08-22 22:57:31 +0530

    Bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
 import Portfolio from "./components/Portfolio";
