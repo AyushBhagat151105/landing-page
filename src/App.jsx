dc2e34d4df545ebb5cef97d84a124c004d5d8ff2
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2024-08-22 20:15:39 +0530

    "Added Hero component to App component"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Navbar from "./components/navbar";
+import Hero from "./components/Hero";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,6 +8,7 @@ function App() {
   return (
     <main className="overflow-x-hidden antialiased text-neutral-800">
       <Navbar />
+      <Hero />
     </main>
   );
 }
